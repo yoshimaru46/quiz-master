d1dbed626afe083195e2042ac5052d0ed6b3d2dd
yshr446 <yshr446@gmail.com>
2018-04-12 07:09:57 +0900

    Add routes for answers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :questions
+      resources :questions, shallow: true do
+        resources :answers, only: %i[index show crate]
+      end
     end
   end
 end
