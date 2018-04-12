aafb31fe945233bbb8ca7c592d5d40578b98001b
yshr446 <yshr446@gmail.com>
2018-04-13 03:49:58 +0900

    Add App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { NavLink, Switch, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+import QuestionListPage from './pages/question-list-page';
+import QuestionFormPage from './pages/question-form-page';
+import AnswerFormPage from './pages/answer-form-page';
+import AnswerPage from './pages/answer-page';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    render() {
+        return (
+            <Container>
+                <div className="ui three item menu">
+                    <NavLink className="item" activeClassName="active" exact to="/">
+                        Questions List
+                    </NavLink>
+                    <NavLink className="item" activeClassName="active" exact to="/questions/new">
+                        Add Question
+                    </NavLink>
+                    <NavLink className="item" activeClassName="active" exact to="/answers/new">
+                        Start Answer
+                    </NavLink>
+                </div>
+                <Switch>
+                    <Route exact path="/" component={QuestionListPage}/>
+                    <Route exact path="/questions" component={QuestionListPage}/>
+                    <Route path="/questions/new" component={QuestionFormPage}/>
+                    <Route path="/questions/edit/:id" component={QuestionFormPage}/>
+                    <Route path="/answers/new" component={AnswerFormPage}/>
+                    <Route path="/answers/:id" component={AnswerPage}/>
+                </Switch>
+            </Container>
+        );
+    }
 }
 
 export default App;
